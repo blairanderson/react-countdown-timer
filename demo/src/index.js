9211c17168f398859750383b9561a62bd3c76c0a
Blair Anderson <Blair81@gmail.com>
2017-07-12 13:33:40 -0700

    add text example

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,10 +5,28 @@ import '../../css/default.css';
 
 class Demo extends Component {
   render() {
+    const date = '06/29/2018 12:00:00';
+    const style = { textAlign: 'center' };
     return (
       <div>
-        <h1 style={{ textAlign: 'center' }}>react-countdown-timer Demo</h1>
-        <Example date={'06/10/2018 12:00:00'} />
+        <h1 style={style}>react-countdown-timer Demo</h1>
+        <h2 style={style}>starting {date}</h2>
+        <Example date={date} />
+        <textarea
+          style={{
+            width: '100%',
+            textAlign: 'center'
+          }}
+          value={'<Example date={"06/29/2018 12:00:00"} />'}
+        />
+        <footer style={style}>
+          <a
+            style={{ color: 'white', fontSize: '20px' }}
+            href="https://github.com/blairanderson/react-countdown-timer/"
+          >
+            GitHub
+          </a>
+        </footer>
       </div>
     );
   }
